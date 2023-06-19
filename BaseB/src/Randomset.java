17532008e6959fdf35ddd8a578858200fdb76730
hyun1024 <haema3043@gmail.com>
2023-06-19 19:24:22 +0900

    랜덤리스트 반복문제거 후 초기화

--- BaseB/src/Randomset.java
+++ BaseB/src/Randomset.java
@@ -2,10 +2,7 @@ import java.util.*;
 public class Randomset {
     public int[] ran (int a) {
         Random ran = new Random();
-        ArrayList<Integer> numList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            numList.add(i);
-        }
+        ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
         int[] targetNum = new int[a];
         for (int i = 0; i < targetNum.length; i++) {
             int n = ran.nextInt(numList.size());
